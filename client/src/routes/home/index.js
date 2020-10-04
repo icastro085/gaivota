bb9cb20c26166c9bf689127824842a7677a8406a
Ivanildo de Castro <ivanildo.decastro085@gmail.com>
2020-10-03 23:30:13 -0400

    feat: adding select component

--- client/src/routes/home/index.js
+++ client/src/routes/home/index.js
@@ -1,11 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Select from '../../components/Select';
 import LeafletMap from '../../components/LeafletMap';
+import api from '../../facades/api';
 
 export default function Home() {
+  const [selectedFarm, setSelectedFarm] = useState({});
+  const [farmsList, setFarmsList] = useState([]);
+  const itemsFarm = farmsList.map(({ farmId, name }) => ({
+    id: farmId,
+    value: name,
+  }));
+
+  const onChangeFarm = (farmId) => {
+    console.log(farmId);
+  };
+
+  useEffect(() => {
+    const loadFarms = async () => {
+      const { data: { items = [] } } = await api.get('/farm');
+      setFarmsList(items);
+      setSelectedFarm(items[0]);
+    };
+
+    loadFarms();
+  }, []);
+
   return (
-    <>
-      Gaivota Test
-      <LeafletMap />
-    </>
+    <div className="row map-container">
+      <div className="col-8">
+        <LeafletMap />
+      </div>
+      <div className="col-4 search-container">
+        <form>
+          <h4>Fazenda</h4>
+
+          {
+            farmsList.length
+              ? (
+                <Select
+                  items={itemsFarm}
+                  value={selectedFarm.farmId}
+                  onChange={onChangeFarm}>
+                  Selecione uma fazenda
+                </Select>
+              )
+              : null
+          }
+        </form>
+      </div>
+    </div>
   );
 }
